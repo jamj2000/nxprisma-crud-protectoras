f5856dec8ea815f32839fe661f76f1037701f8ca
José A. Muñoz Jiménez <jamj2000@gmail.com>
2025-01-04 16:15:19 +0100

    eliminar vacuna!

--- src/app/vacunas/[id]/page.jsx
+++ src/app/vacunas/[id]/page.jsx
@@ -1,5 +1,9 @@
+import Modal from "@/components/Modal";
+import VacunaEliminar from "@/components/Vacunas/Eliminar";
+import VacunaModificar from "@/components/Vacunas/Modificar";
 import VacunaVer from "@/components/Vacunas/Ver";
 import { obtenerVacuna } from "@/lib/data";
+import { Pencil, Trash } from "lucide-react";
 import { notFound } from "next/navigation";
 
 
@@ -9,7 +13,25 @@ async function page({ params }) {
 
     if (!vacuna) notFound()
 
-    return <VacunaVer vacuna={vacuna} />
+    return (
+        <>
+            <div className='flex gap-1 justify-end'>
+                <Modal
+                    icono={<Pencil />}
+                    className={'place-self-end p-1 rounded-full border border-orange-500 text-orange-700 bg-orange-200 hover:bg-orange-500 hover:text-white hover:cursor-pointer'}>
+                    <VacunaModificar vacuna={vacuna} />
+                </Modal>
+
+                <Modal
+                    icono={<Trash />}
+                    className={'place-self-end p-1 rounded-full border border-red-500 text-red-700 bg-red-200 hover:bg-red-500 hover:text-white hover:cursor-pointer'}>
+                    <VacunaEliminar vacuna={vacuna} />
+                </Modal>
+
+            </div>
+            <VacunaVer vacuna={vacuna} />
+        </>
+    )
 }
 
 
