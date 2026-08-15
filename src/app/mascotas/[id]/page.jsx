5d6752a8085205e9a3d1eea95fe6a5de43de7965
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2026-08-15 02:20:27 +0200

    Revert "ViewTransition from React"
    This reverts commit d01fb2c295cb90086b13e353f761b16e2b426ec8.

--- src/app/mascotas/[id]/page.jsx
+++ src/app/mascotas/[id]/page.jsx
@@ -3,7 +3,7 @@ import { DeleteMascota, UpdateMascota } from '@/app/mascotas/components'
 import { getMascota, getMascotasIdNombre } from '@/app/mascotas/data'
 import { getProtectorasIdNombre } from '@/app/protectoras/data'
 import { getVacunasIdNombre } from '@/app/vacunas/data'
-import { Suspense, ViewTransition } from "react"
+import { Suspense } from "react"
 import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
 import BackButton from '@/components/back-button'
@@ -59,9 +59,7 @@ const Content = async ({ params }) => {
                 <ArrowLeft />
             </BackButton>
 
-            <ViewTransition name={`mascota-foto-${mascota.id}`} share="morph">
-                <img src={mascota.foto} className='mx-auto' />
-            </ViewTransition>
+            <img src={mascota.foto} className='mx-auto' style={{ viewTransitionName: `mascota-foto-${mascota.id}` }} />
 
             <div className='flex justify-between mt-10'>
 
