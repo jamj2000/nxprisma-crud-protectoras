1dd3f5e23459c48bad8ffbd726550fa7419bb22f
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2026-08-23 22:17:04 +0200

    prisma

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,21 +1,16 @@
-import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@/generated/prisma/client';
 
-const globalForPrisma = globalThis;
-
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-});
+const globalForPrisma = globalThis
 
 const prisma =
-    globalForPrisma.prisma ||
+    globalForPrisma.prisma ??
     new PrismaClient({
-        adapter,
+        adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
     });
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
     globalForPrisma.prisma = prisma;
 }
 
-export default prisma;
-
+export default prisma
